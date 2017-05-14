9047eb11d99f6f6e556260945773cbc81d53ecea
KY <infcurious@gmail.com>
2017-05-14 22:07:21 +0800

    First iOS app

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Ting Kher Yee on 14/5/17.
+//  Copyright © 2017 Janitor co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello iOS!", message: "This is my first iOS app", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
+}
+
